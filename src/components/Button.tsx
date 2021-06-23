5ef83c88ca14f44d74baec1516b8754c6ad328a5
Fabiano Junqueira <fabiano@pdsolucoes.com.br>
2021-06-22 21:33:09 -0300

    feat: :art: Aula 2 finalizada.
    Utilizando React, trabalhamos contexto, fire base, estado, propriedade autendicação entre outras coisas.

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { type } from "os"
+import { ButtonHTMLAttributes } from "react"
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+    return (
+        <button className="button" {...props} />
+    )
+}
